0c972e40b73cd265b64cfd1ba3a8bd99965450bd
Victor MIranda <victormiranda@gmail.com>
2016-05-13 08:23:27 +0100

    extract transactions from the accounts
    they are removed from the paylod in jackson in order to avoid circular references

--- src/main/java/com/victormiranda/mani/bean/SynchronizationResult.java
+++ src/main/java/com/victormiranda/mani/bean/SynchronizationResult.java
@@ -4,23 +4,31 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class SynchronizationResult {
 
 	private final Set<AccountInfo> accounts;
+	private final List<Transaction> transactions;
 	private final Boolean syncDone;
 
 	@JsonCreator
 	public SynchronizationResult(
 			@JsonProperty("accounts") final Set<AccountInfo> accounts,
+			@JsonProperty("transactions") final List<Transaction> transactions,
 			@JsonProperty("syncDone") final boolean syncDone) {
 		this.accounts  = accounts;
+		this.transactions = transactions;
 		this.syncDone = syncDone;
 	}
 
 	public Set<AccountInfo> getAccounts() {
 		return accounts;
+	}
+
+	public List<Transaction> getTransactions() {
+		return transactions;
 	}
 
 	public Boolean getSyncDone() {
@@ -31,6 +39,7 @@ public class SynchronizationResult {
 	public String toString() {
 		return "SynchronizationResult{" +
 				"accounts=" + accounts +
+				"transactions=" + transactions +
 				", syncDone=" + syncDone +
 				'}';
 	}
